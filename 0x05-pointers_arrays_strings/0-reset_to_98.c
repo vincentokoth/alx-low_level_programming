14790fd72797d28b31de2481611ede81c0967ca9
vincentokoth <vincent.okoth856@gmail.com>
2021-06-28 10:59:50 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Okoth Vincent Odhiambo
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
+
+
