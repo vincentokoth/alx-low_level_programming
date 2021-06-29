538e648ff6260fe626b5e078fcbce684ac922733
vincentokoth <vincent.okoth856@gmail.com>
2021-06-29 00:32:17 -0700

    First commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * Task: 2-strlen.c
+ *
+ * Author: Okoth Vincent Odhiambo
+ */
+
+int main(void)
+{
+  char *str;
+    int len;
+
+    str = "Holberton!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
