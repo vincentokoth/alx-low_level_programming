58e8a73eb11355c7cdf036f63572f0d248dece69
vincentokoth <vincent.okoth856@gmail.com>
2021-07-27 12:04:47 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/*Author: Vincent Okoth*/
+
+#include <stdio.h>
+
+/**
+ *main- Prints the alphabet in lowercase, followed by a new line.
+ *
+ *Return: 0 if output is correct.
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		if ((lc == 'e') || (lc == 'q'))
+		    continue;
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+
+	return (0);
+}
