317f3dd479120226bf17a6106a767c11efaf863d
vincentokoth <vincent.okoth856@gmail.com>
2021-08-29 20:37:46 -0700

    Modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 /*
- *File: holberton.h
+ *File: main.h
  *Author: Vincent Okoth
  *Description: Header file containing declarations for all functions
  * used in the 0x02-functions_nested_loops directory.
