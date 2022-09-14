10e3be6f94699dcf3842368a7e2a1976469cd8e4
root <vincent.okoth856@gmail.com>
2022-09-14 14:47:54 -0700

    Modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,24 @@
 /*
  * File: 1-alphabet.c
  * Author: Vincent Okoth
- * Description: A function that prints the alphabet, in lowercase, followed by a new line.
- *
+ */
+
+/**
+ * print_alphabet- a function that prints the alphabet, in lowercase,
+ * followed by a new line.
  */
 
 #include "main.h"
 
 void print_alphabet(void)
 {
-	char ltr= 'a';
+	char ltr = 'a';
 
 	while (ltr <= 'z')
 	{
 		_putchar(ltr);
 		ltr++;
-	}
 	_putchar('\n');
-}
+	}
 
+}
