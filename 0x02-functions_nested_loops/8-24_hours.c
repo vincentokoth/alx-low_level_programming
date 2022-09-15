fe39bf5f6d5afe135f994d7a81e9bf2f16883ad3
root <vincent.okoth856@gmail.com>
2022-09-14 18:17:26 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-24_hours.c
+ * Author: Vincent Okoth
+ */
+
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer,
+ *		starting from 00:00 to 23:59
+ */
+
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
