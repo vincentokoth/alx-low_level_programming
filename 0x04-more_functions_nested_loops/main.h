1ec29522d92f961f2de59273c5dfce6af0b857e0
root <vincent.okoth856@gmail.com>
2022-09-15 13:13:17 -0700

    Updated with _putchar prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
  * Description: Header file containingthe prototypes for all functions
  *		listed in the 0x04-more_functions_nested_loops directory
  */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
