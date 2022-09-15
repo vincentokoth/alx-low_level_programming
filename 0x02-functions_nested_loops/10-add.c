62da4bb31a09a73ea959ca6db802ebab48b04747
root <vincent.okoth856@gmail.com>
2022-09-14 18:50:18 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Author: Vincent Okoth
+ */
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ * Return: The result of the addition.
+ */
+
+#include "main.h"
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
