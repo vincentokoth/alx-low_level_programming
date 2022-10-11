aaa51492d96bfc29a8179c2811039c16201be985
root <vincent.okoth856@gmail.com>
2022-10-11 13:45:27 -0700

    Initial Commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-free_dog.c
+ * Author: 5-free_dog.c
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
