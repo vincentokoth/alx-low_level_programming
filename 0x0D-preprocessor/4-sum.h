05ca9237581012e07a2fbfd28587eecddc0dfb64
root <vincent.okoth856@gmail.com>
2022-10-12 06:39:36 -0700

    Initial Commit

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Author: Vincent Okoth
+ * Desc: A header file that defines a function-like macros
+ *	SUM(x, y) that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
